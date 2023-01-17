59c1998711d1caf38f3b31989c1a1c14f8019822
Sam Chappel <m.samchappel@gmail.com>
2023-01-17 06:46:36 -0800

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+// document.addEventListener("keydown", function (event) {
+//     if (event.key === "ArrowLeft") {
+//       const leftNumbers = dodger.style.left.replace("px", "");
+//       const left = parseInt(leftNumbers, 10);
+  
+//       dodger.style.left = `${left - 1}px`;
+//     }
+//   });
+
+
+
+  const dodger = document.getElementById("dodger");
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    dodger.style.left = `${left + 1}px`;
+}
+
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
+
+  dodger.style.backgroundColor = "#FF69B4";
+  dodger.style.backgroundColor = "#FF69B4";dodger.style.bottom = "100px";
+  dodger.style.bottom = "0px";
+//   dodger.style.left = "0px";
